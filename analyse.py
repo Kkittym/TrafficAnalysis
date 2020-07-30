004acf3706b3cbde3c00a0b83c8600053f087502
Katie <katie.midgley@ntlworld.com>
2020-07-30 20:02:11 +0100

    Additional analysis as required

--- analyse.py
+++ analyse.py
@@ -34,33 +34,21 @@ for i in range(1,data.shape[0]):
 
 cars.append(max(car))
 
-print('')
-print(cars)
-print('num cars =', len(cars))
-print('Average speed =', sum(cars)/len(cars))
-print('Total over 20 =', len([i for i in cars if i > 20 and i <= 30]))
-print('Total over 30 =', len([i for i in cars if i > 30 and i <= 40]))
-print('Total over 40 =', len([i for i in cars if i > 40 and i <= 50]))
-print('Total over 50 =', len([i for i in cars if i > 50]))
-print('Max speed =', max(cars))
-
 numCars = len(cars)
 aveSpeed = sum(cars)/len(cars)
 over20 = len([i for i in cars if i > 20 and i <= 30])
 over30 = len([i for i in cars if i > 30 and i <= 40])
 over40 = len([i for i in cars if i > 40 and i <= 50])
-over50 = len([i for i in cars if i > 50])
+over50 = len([i for i in cars if i > 50 and i <= 60])
+over60 = len([i for i in cars if i > 60 and i <= 70])
+over70 = len([i for i in cars if i > 70])
 maxSpeed = max(cars)
-d = {'Number of cars':numCars,'Average Speed':aveSpeed,'Total over 20 and less than 30':over20,'Total over 30 and less than 40':over30,'Total over 40 and less than 50':over40,'Total over 50':over50,'Max speed in the month':maxSpeed}
+d = {'Number of cars':numCars,'Average Speed':aveSpeed,'Total over 20 and less than 30':over20,'Total over 30 and less than 40':over30,'Total over 40 and less than 50':over40,'Total over 50 and less than 60':over50,'Total over 60 and less than 70':over60,'Total over 70':over70,'Max speed in the month':maxSpeed}
 data = pd.DataFrame(data=d, index=[month])
-print(data, '\n')
 
-out = None
 try:
     out = pd.read_excel('out.xlsx', index_col=0)
-    print(out, '\n')
     out = out.append(data)
-    print(out, '\n')
     out.to_excel('out.xlsx')
 except FileNotFoundError:
     data.to_excel('out.xlsx')
